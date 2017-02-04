f7e61416937b9f8b6cc1a7bf9176af23f60eded1
Filip Matiaska <matiaskafi@gmail.com>
2017-02-04 16:44:48 +0100

    Zakladni soubory sablony vytvoreny

---
+++ dist/index.php
@@ -0,0 +1,21 @@
+<?php
+	
+	get_header();
+
+	if(have_posts()) {
+		while(have_posts()) {
+			the_post();
+			?>
+
+				<h2>Title should go here</h2>
+
+			<?php
+
+		}
+	} else {
+		echo("Žádné příspěvky :-)");
+	}
+
+	get_footer();
+
+?>
