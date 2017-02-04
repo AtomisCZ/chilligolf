bc9f245815768cde146c7c35997de26bd5788519
Filip Matiaska <matiaskafi@gmail.com>
2017-02-04 15:03:12 +0100

    Pridan gulp plugin - gulp-ftp, vlozeny pristupove udaje k testovaci wordpress strance

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var uglify = require('gulp-uglifyjs');
+var ftp = require('gulp-ftp');
+var gutil = require('gulp-util');
 
 
 
@@ -16,4 +18,21 @@ gulp.task('serve', function() {
     	.pipe(sass().on('error', sass.logError))
    		.pipe(cleanCSS({compatibility: 'ie8'}))
     	.pipe(gulp.dest('dist'));
+
+    gulp.src('app/*.php')
+    	.pipe(sass().on('error', sass.logError))
+   		.pipe(cleanCSS({compatibility: 'ie8'}))
+    	.pipe(gulp.dest('dist'));
 });
+
+gulp.task('ftp', function () {
+    return gulp.src('dist/sopoto.txt')
+        .pipe(ftp({
+            host: 'apitest.borec.cz',
+            user: 'apitest.borec.cz',
+            pass: '8dg3c3u6',
+            remotePath: '/js'
+        }))
+        .pipe(gutil.noop());
+});
+
